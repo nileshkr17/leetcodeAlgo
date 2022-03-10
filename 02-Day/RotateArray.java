e3bc56dd933237f9b6d5e551e511d45ea6049e31
Nilesh <ece.20becd76@silicon.ac.in>
2022-03-10 20:54:52 +0530

    [02-Day]

--- 02-Day/RotateArray.java
+++ 02-Day/RotateArray.java
@@ -1,3 +1,33 @@
-public class RotateArray {
+class Solution {
     
+    void reverse(int arr[], int  low,int high )
+    {
+       int tmp;
+        
+        while(low<high)
+        {
+           tmp = arr[low];
+            arr[low]=arr[high];
+            arr[high]=tmp;
+            low++;
+            high--;
+        }
+        
+    }
+   
+   public void rotate(int[] nums, int k) {
+       
+       int n = nums.length;
+       if(n==1)
+       return;
+       
+       k=k%n;
+       
+       reverse(nums, 0,n-k-1);
+       reverse(nums,n-k,n-1);
+       reverse(nums,0,n-1);
+       
+       
+   }
+   
 }
