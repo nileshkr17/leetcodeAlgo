4efd9568c965945145f705824ea62a7df77eab62
Nilesh <ece.20becd76@silicon.ac.in>
2022-03-13 23:38:39 +0530

    [04-Day]

---
+++ 04-Day/rString.java
@@ -0,0 +1,14 @@
+class Solution{
+    public void rString(char[] s)
+    {
+        int len = s.length;
+        char temp;
+        for(int i = 0; i<len/2; ++i)
+        {
+            temp=s[i];
+            s[i]=s[len-i-1];
+            s[len-i-1]=temp;
+            
+        }
+    }
+}
