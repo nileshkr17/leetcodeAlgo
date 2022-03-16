da6a224b7db4c852a4d26cefcb77da43f6cd7733
Nilesh <ece.20becd76@silicon.ac.in>
2022-03-16 23:37:14 +0530

    [ValidPalin]

---
+++ Java/validPalin.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s == null || s.length() == 0)
+            return true;
+        
+        s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "").strip();
+        
+        int start = 0, end = s.length() - 1;
+        
+        while(start <= end)
+        {
+            if(s.charAt(start) == s.charAt(end))
+            {
+                ++start;
+                --end;
+            }
+            else
+                return false;
+        }
+        
+        return true;
+    }
+}
